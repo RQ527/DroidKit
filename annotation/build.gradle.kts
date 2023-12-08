54a264ba9515fd023b36f71ffaf6093fc754873d
rsix <ransixiang@bilibili.com>
2023-12-08 12:01:54 +0800

    :bug: 修复jitpack检测出错

--- annotation/build.gradle.kts
+++ annotation/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 apply("./stub-lib.gradle.kts")
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(8)
 }
 dependencies{
     val stub = tasks.named<Jar>("stubLibsJar").get().outputs.files
--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
     id("kotlin-kapt")
 }
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(8)
 }
 dependencies {
     implementation(project(":annotation"))
